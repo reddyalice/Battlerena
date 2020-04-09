fcecb4c1e18afca519abbbe0df1ab7ca27254454
Alice <kodekishin@gmail.com>
2020-04-09 18:39:55 +0300

    Initial GDX Setups

---
+++ desktop/src/com/alice/arena/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alice.arena.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alice.arena.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}

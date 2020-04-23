7d594cada2a4415005c1c801adc08b5206f8d6fd
Alice <kodekishin@gmail.com>
2020-04-24 02:40:55 +0300

    LWJGL3 update

--- desktop/src/com/alice/arena/desktop/DesktopLauncher.java
+++ desktop/src/com/alice/arena/desktop/DesktopLauncher.java
@@ -2,13 +2,15 @@ package com.alice.arena.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.alice.arena.Core;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
-		config.height = 768;
-		new LwjglApplication(new Core(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1024, 768);
+		config.useVsync(true);
+		new Lwjgl3Application(new Core(), config);
 	}
 }
